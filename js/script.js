fcff916870f10dafbac8dda0ed4bbd5628c82eeb
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-28 18:19:31 +0100

    project setup done

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// 1 - permettere l'inserimento con un prompt del numero di chilometri che si intende percorrere
+
+
+
+// 2 - pemettere l'inserimento con um prompt della prorpria età
+
+
+
+// 3 - moltiplicare il prezzo di 0.21€ per il numero di chilometri inseriti dall'utente
+
+
+
+// 4 - calcolare gli eventuali sconti sul prezzo ottenuto in base all'età inserita dall'untente
+
+	// 4.1 - se l'età è inferiore ai 18 anni applicare uno sconto del 20%
+
+
+
+	// 4.2 - se l'età è superiore ai 65 anni applicare uno sconto del 40%
+
+
+
+	// 4.3 - se l'età è compresa tra i 18 e i 65 anni non applicare nessuno sconto
---
+++ readme.txt
@@ -0,0 +1,7 @@
+1 - chiedere all'utente di inserire il numero di chilometri che intende percorrere
+2 - chiedere all'utente di inserire la prorpria età
+3 - moltiplicare il prezzo di 0.21€ per il numero di chilometri inseriti dall'utente
+4 - calcolare gli eventuali sconti sul prezzo ottenuto in base all'età inserita dall'utente
+	4.1 - se l'età è inferiore ai 18 anni applicare uno sconto del 20%
+	4.2 - se l'età è superiore ai 65 anni applicare uno sconto del 40%
+	4.3 - se l'età è compresa tra i 18 e i 65 anni non applicare nessuno sconto
