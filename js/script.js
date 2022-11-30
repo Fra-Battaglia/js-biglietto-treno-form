d6baf49c1cd80a7d47d5c54256194207ec74f634
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-30 18:17:07 +0100

    form done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
-// 1 - permettere l'inserimento con un prompt del numero di chilometri che si intende percorrere
+const price_button = document.getElementById('calc-price');
+price_button.addEventListener('click', function(){
 
+	// 1 - chiedere il nome e cognome del passeggero
+	// 1.1 - pemettere l'inserimento con un input del prorprio nome e cognome
+	
+	let name_and_surname = document.getElementById('name_and_surname').value;
+	
+	// 2 - chiedere l'età del passeggero
+	// 2.1 - pemettere l'inserimento con un input della prorpria età
+	
+	let age = document.getElementById('age').value;
+	
+	// 3 - chiedere il numero di chilometri che si intende percorrere
+	// 3.1 - permettere l'inserimento con un input del numero di chilometri che si intende percorrere
 
+	let num_km = document.getElementById('num_km').value;
 
-// 2 - pemettere l'inserimento con um prompt della prorpria età
+	// 4 - moltiplicare il prezzo di 0.21€ per il numero di chilometri inseriti dall'utente
 
+	let price = num_km * 0.21;
 
+	// 5 - calcolare gli eventuali sconti sul prezzo ottenuto in base all'età inserita dall'untente
+	// 5.1 - se l'età è inferiore ai 18 anni applicare uno sconto del 20%
 
-// 3 - moltiplicare il prezzo di 0.21€ per il numero di chilometri inseriti dall'utente
+	if (age < 18) {
+		price = (price * 0.8).toFixed(2);
+	}
 
+	// 5.2 - se l'età è superiore ai 65 anni applicare uno sconto del 40%
 
+	else if (age > 65) {
+		price = (price * 0.6).toFixed(2);
+	}
 
-// 4 - calcolare gli eventuali sconti sul prezzo ottenuto in base all'età inserita dall'untente
+	// 5.3 - se l'età è compresa tra i 18 e i 65 anni non applicare nessuno sconto
 
-	// 4.1 - se l'età è inferiore ai 18 anni applicare uno sconto del 20%
+	else if (age >= 18 && age < 65) {
+		price = (price).toFixed(2);
+	}
 
+	// 6 - Stampare le informazioni sul biglietto
 
-
-	// 4.2 - se l'età è superiore ai 65 anni applicare uno sconto del 40%
-
-
-
-	// 4.3 - se l'età è compresa tra i 18 e i 65 anni non applicare nessuno sconto
+	document.getElementById('put-name').innerText = name_and_surname;
+	document.getElementById('put-price').innerText = price;
+});
